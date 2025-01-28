f633145a65bfcd83516e32f91792cca28b5a2641
PearceChen42 <cboxiang97@gmail.com>
2025-01-29 01:17:23 +0800

    complate tasks app

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,11 +1,34 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect 
+from django.urls import reverse
 
-task = ["harry", "ron", "kali", "foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     print("start tasks")
     return render(request, "tasks/index.html",{
-        "names": task
+        "names": request.session["tasks"]
     }
   )
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
