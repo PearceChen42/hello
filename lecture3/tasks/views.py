32138e6c58bf1b524ef5ee57b276c8e165fcf013
PearceChen42 <cboxiang97@gmail.com>
2025-01-28 17:31:41 +0800

    fix a bug in tasks

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -4,6 +4,8 @@ tasks = ["foo", "bar", "baz"]
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html", {
+    print("start tasks")
+    return render(request, "tasks/index.html",{
         "tasks": tasks
-    })
+    }
+  )
