c224d602cb311754ea56086b464380fc9b625f64
PearceChen42 <cboxiang97@gmail.com>
2025-02-05 19:14:02 +0800

    Fix some error in test

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from .models import Flight, Passenger
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 
 # Create your views here.
@@ -13,13 +13,16 @@ def index(request):
 
 
 def flight(request, flight_id):
-    flight = Flight.objects.get(pk=flight_id)
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+    except: 
+        raise Http404("Flight does not exist")
     return render(request, "flights/flight.html", {
         "flight": flight,
         "passengers": flight.passengers.all(),
         "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
-
+    
 
 def book(request,flight_id):
     if request.method == "POST":
