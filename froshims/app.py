cbaa39cd3861f68c552e4d95abe9b9ddf4e4cd37
PearceChen42 <cboxiang97@gmail.com>
2025-02-08 05:58:22 +0800

    Add a froshims app in Flask

---
+++ froshims/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+from cs50 import SQL
+
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///froshims.db")
+
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Ultimate Frisbsee",
+]
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+    
+    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
+    
+    return render_template("success.html")
+
+@app.route("/registrants")
+def registrants():
+    registrants = db.execute("SELECT name, sport FROM registrants")
+    print(registrants)
+    return render_template("registrants.html", registrants=registrants)
