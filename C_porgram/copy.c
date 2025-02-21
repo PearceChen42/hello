bb0c34d9b69f8613588d3d61f59eef6b6c9e88ab
PearceChen42 <cboxiang97@gmail.com>
2025-02-22 02:48:10 +0800

    Fix one bug in copy.c

--- C_porgram/copy.c
+++ C_porgram/copy.c
@@ -2,11 +2,12 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
     char *s = get_string("s: ");
-    char *t = malloc(4);
+    char *t = malloc(strlen(s) + 1);
     for ( int i = 0; i <= strlen(s); i++)
     {
         t[i] = s[i];
