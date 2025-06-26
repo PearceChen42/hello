1145d2375499f527ae83c76b5a08412cf5367141
PearceChen42 <cboxiang97@gmail.com>
2025-06-27 02:16:42 +0800

    Java labels

--- ECLIPSEWORK/JavaLabels/src/Main.java
+++ ECLIPSEWORK/JavaLabels/src/Main.java
@@ -1,10 +1,43 @@
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.*;
+import javax.swing.border.Border;
 public class Main {
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ImageIcon image = new ImageIcon("logo.png");
+		Border border = BorderFactory.createLineBorder(Color.green,3);
+		
+		
+		JLabel label = new JLabel();
+		label.setText("Bro, do you even code? ");
+	
+		label.setIcon(image);
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		label.setVerticalTextPosition(JLabel.BOTTOM);
+		label.setForeground(new Color(0x00FF00));
+		label.setFont(new Font("MV Boli",Font.BOLD,20));
+		label.setIconTextGap(-25);
+		label.setBackground(Color.black);
+		label.setOpaque(true);
+		label.setBorder(border);
+		label.setVerticalAlignment(JLabel.CENTER);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		//label.setBounds(0,0,620,620);
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.setSize(620,620);
+		//frame.setLayout(null);
+		frame.setVisible(true);
+		frame.add(label);
+		frame.pack();
+		
+		
 	}
 
 }
