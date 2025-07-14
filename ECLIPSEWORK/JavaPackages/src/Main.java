92fd85f1d8721d8b570d28b308c24748accc5edd
PearceChen42 <cboxiang97@gmail.com>
2025-07-14 17:23:39 +0800

    JavaOTP

---
+++ ECLIPSEWORK/JavaPackages/src/Main.java
@@ -0,0 +1,15 @@
+import Tools.*;
+
+import javax.swing.Icon;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Toolbox toolbox = new Toolbox();
+		
+	}
+
+}
